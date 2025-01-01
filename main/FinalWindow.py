8221326a815dc427203fd28b96f510b0715ba79f
RoeunApireak <apireak.roeun.datau@gmail.com>
2025-01-01 15:46:16 +0700

    part_2

--- main/FinalWindow.py
+++ main/FinalWindow.py
@@ -1,3 +1,29 @@
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout
+from instr import *
+
+class FinalWin(QWidget):
+    def __init__(self, exp):
+        super().__init__()
+
+        self.exp = exp
+
+        self.set_appear()
+        self.initUI()
+
+        self.show()
+
+    def set_appear(self):
+        self.setWindowTitle('Result')
+        self.resize(window_widht, window_height)
+        self.move(window_x, window_y)
+    
+    def initUI(self):
+        self.work_text = QLabel('Cardiac performance: ' + '0')
+        self.index_text = QLabel('Roufier Index: ' + '0')
+
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.index_text, alignment = Qt.AlignCenter)
+        self.layout_line.addWidget(self.work_text, alignment = Qt.AlignCenter)        
+        self.setLayout(self.layout_line)
--- main/TestWindow.py
+++ main/TestWindow.py
@@ -1,7 +1,16 @@
 
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTime, QTimer
 from PyQt5.QtWidgets import QWidget, QPushButton, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout
+from PyQt5.QtGui import QFont
 from instr import *
+from FinalWindow import FinalWin
+
+class Experiment():
+   def __init__(self, age, test1, test2, test3):
+       self.age = age
+       self.t1 = test1
+       self.t2 = test2
+       self.t3 = test3
 
 class TestWin(QWidget):
     def __init__(self):
@@ -27,6 +36,7 @@ class TestWin(QWidget):
         self.name_txt = QLabel(name_txt, self)
         self.age_txt = QLabel(age_txt, self)
         self.timer_txt = QLabel(timer_txt, self)
+        self.timer_txt.setFont(QFont("Times", 36, QFont.Bold))
         self.test_txt_1 = QLabel(test_txt_1, self)
         self.test_txt_2 = QLabel(test_txt_2, self)
         self.test_txt_3 = QLabel(test_txt_3, self)
@@ -44,9 +54,49 @@ class TestWin(QWidget):
         self.r_v_line.addWidget(self.timer_txt, alignment=Qt.AlignCenter)
         self.l_v_line.addWidget(self.name_txt, alignment=Qt.AlignLeft)
         self.l_v_line.addWidget(self.name_line, alignment=Qt.AlignLeft)
-        
-        ##
+        self.l_v_line.addWidget(self.age_txt, alignment = Qt.AlignLeft)
+        self.l_v_line.addWidget(self.hint_test_age, alignment = Qt.AlignLeft)
+        self.l_v_line.addWidget(self.test_txt_1 , alignment = Qt.AlignLeft)
+        self.l_v_line.addWidget(self.start_test_1_btn , alignment = Qt.AlignLeft)
+        self.l_v_line.addWidget(self.hint_test_1 , alignment = Qt.AlignLeft)
+        self.l_v_line.addWidget(self.test_txt_2 , alignment = Qt.AlignLeft)
+        self.l_v_line.addWidget(self.start_test_2_btn , alignment = Qt.AlignLeft)
+        self.l_v_line.addWidget(self.hint_test_2 , alignment = Qt.AlignLeft)
+        self.l_v_line.addWidget(self.test_txt_3 , alignment = Qt.AlignLeft)
+        self.l_v_line.addWidget(self.start_test_3_btn , alignment = Qt.AlignLeft)
+        self.l_v_line.addWidget(self.hint_test_3 , alignment = Qt.AlignLeft)
+        self.l_v_line.addWidget(self.sendresult_btn , alignment = Qt.AlignCenter)
+
         self.h_line.addLayout(self.l_v_line)
+        self.h_line.addLayout(self.r_v_line)
         self.setLayout(self.h_line)
         
+    def click_next(self):
+        self.hide()
+        self.exp = Experiment(age=int(self.hint_test_age.text()), 
+                              test1=self.hint_test_1.text(), 
+                              test2=self.hint_test_2.text(), 
+                              test3=self.hint_test_3.text())
+        self.final_result = FinalWin(exp=self.exp)
 
+    def time_test(self):
+        global time
+        time = QTime(0, 0, 15) # https://doc.qt.io/qtforpython-5/PySide2/QtCore/QTime.html#
+        self.timer = QTimer() # https://doc.qt.io/qtforpython-5/PySide2/QtCore/QTimer.html
+        self.timer.timeout.connect(self.timer1Event)
+        self.timer.start(1000)
+
+    def timer1Event(self):
+        global time
+        time = time.addSecs(-1) # https://doc.qt.io/qtforpython-5/PySide2/QtCore/QTime.html#PySide2.QtCore.PySide2.QtCore.QTime.addSecs
+        self.timer_txt.setText(time.toString("hh:mm:ss"))
+        self.timer_txt.setFont(QFont("Times", 36, QFont.Bold))
+        self.timer_txt.setStyleSheet("color: rgb(0,0,0)")
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timer.stop()
+
+    def connects(self):
+        self.sendresult_btn.clicked.connect(self.click_next)
+        self.start_test_1_btn.clicked.connect(self.time_test)
+        # self.start_test_2_btn.clicked.connect()
+        # self.start_test_3_btn.clicked.connect()
